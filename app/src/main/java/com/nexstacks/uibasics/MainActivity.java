3c0ca8ccf12d3444aaa197a9050c19a49c3eaa1d
Akash Gautam <gautamakash91@gmail.com>
2020-10-12 21:23:34 +0530

    12Oct2020 Changes

--- app/src/main/java/com/nexstacks/uibasics/MainActivity.java
+++ app/src/main/java/com/nexstacks/uibasics/MainActivity.java
@@ -3,12 +3,44 @@ package com.nexstacks.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvSetupWizard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_constraint);
+
+        mTvSetupWizard = findViewById(R.id.tv_setup_plan);
+
+        mTvSetupWizard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToastMessage();
+            }
+        });
+    }
+
+
+    private void showToastMessage(){
+
+    }
+
+
+    class Student{
+        String studentName;
+        String studentRollNo;
+
+        public void setStudentDetails(){
+
+        }
+
+        public Student(){
+
+        }
     }
 }
